fa7f6fd006ab1e897cf628c3422b10800df59c16
youron1115 <43118045aaa@gmail.com>
2025-04-15 09:50:10 +0800

    check which are right model_train and test whole pipeline

--- model_train_RDI.py
+++ model_train_RDI.py
@@ -3,8 +3,6 @@ import numpy as np
 import pandas as pd
 import h5py
 
-from sklearn.model_selection import train_test_split
-
 import tensorflow as tf
 from keras import layers, models
 
@@ -15,9 +13,13 @@ def fit_model(data,labels,model_path):
     height = 32
     width = 32
     input_shape = (width, height, time_steps, 1)
-    num_classes= 1  #可設定num_classes 種手勢
+    num_classes= 2  #可設定num_classes 種手勢
     
     model = models.Sequential()
+    
+    model.add(layers.TimeDistributed(layers.Conv2D(32, (3, 3), activation='relu'),input_shape=input_shape))
+    model.add(layers.TimeDistributed(layers.MaxPooling2D((3, 3))))
+    #model.add(layers.Dropout(0.3)) 
     
     model.add(layers.TimeDistributed(layers.Conv2D(32, (3, 3), activation='relu'),input_shape=input_shape))
     model.add(layers.TimeDistributed(layers.MaxPooling2D((2, 2))))
@@ -34,15 +36,17 @@ def fit_model(data,labels,model_path):
     model.add(layers.LSTM(64))#64:LSTM units
     model.add(layers.Dense(32, activation='relu'))
     
-    model.add(layers.Dense(num_classes, activation='sigmoid')) 
+    model.add(layers.Dense(num_classes, activation='softmax')) 
 
-    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-    
+    #model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
     model.fit(data, labels, epochs=20, batch_size=14, validation_split=0.3, shuffle=True)
     print("\nTraining complete")
     
+    #model.save(os.path.join(model_path, 'gesture_model_RDI_data_2conv_drop.h5'))
     model.save(os.path.join(model_path, 'gesture_model_RDI_data.h5'))
     print("\nsave complete")
+
 
 current_path = os.path.dirname(os.path.abspath(__file__))
 
--- predict_and_show_RDI.py
+++ predict_and_show_RDI.py
@@ -4,26 +4,26 @@ import os
 
 from keras import models
 
+
 def load_and_predict(model, data):
 
-    predictions = model.predict(data)
-    return predictions
+     predictions = model.predict(data)
+     return predictions
 
 current_path = os.path.dirname(os.path.abspath(__file__))
-
 processed_data_path=os.path.join(current_path, "processed_data")
 train_or_test_data='test.npz'
 test_data = np.load(os.path.join(processed_data_path, train_or_test_data))
 test_labels = test_data['labels']
 test_data = test_data['data']
 
-#model_path=r"D:\mmWave_gesture\data_and_model\model\finally_model\gesture_model_RDI_data_2conv_drop.h5"
-
-model_path = os.path.join(current_path , "model/gesture_model_RDI_data.h5")
+model_path =os.path.join(current_path , "model/gesture_model_RDI_data.h5")
 load_model=models.load_model(model_path)
+
 predicted_value = load_and_predict(load_model, test_data)
 
-output=pd.DataFrame(columns=['Predicted','Actual','Right or False','Predicted Value'])
+
+output=pd.DataFrame(columns=['Predicted','Actual','Right or False','Predicted Probability Value'])
 
 pred_labels = np.argmax(predicted_value, axis=1)
 output['Predicted'] = pred_labels
@@ -47,4 +47,3 @@ print("Predicted Probability Value")
 output_path=os.path.join(current_path, "test_output/predicted_labels_RDI.csv")
 output.to_csv(output_path, index=False)
 print("Predicted {} labels saved to {} CSV file.".format(train_or_test_data,output_path))
-
